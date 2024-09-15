8b840fb5eb3d762a34fd324c0705c5d2297bef9b
ch-iv <alicesummers3000@gmail.com>
2024-09-15 06:47:42 -0400

    Improve design

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,57 @@
-<div class="text-4xl">
-	{text}
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Patrick+Hand:wght@400&display=swap');
+	.text-ferney {
+		font-family: 'Patrick Hand', cursive;
+		line-height: 2; /* Adjust line-height to ensure text isn't cut off */
+	}
+	.animate-gradient {
+		background: linear-gradient(45deg, #a8caba, #d4e9e1, #a1c4fd);
+		-webkit-background-clip: text;
+		-webkit-text-fill-color: transparent;
+		animation: gradient 6s ease infinite;
+	}
+	@keyframes gradient {
+		0% { background-position: 0% 0%; }
+		50% { background-position: 100% 100%; }
+		100% { background-position: 0% 0%; }
+	}
+</style>
+
+<div class="bg-gradient-to-t from-sky-400 to-sky-500 flex items-center justify-center min-h-screen w-full">
+		<h1 class="text-ferney text-[200px] font-bold text-white">
+			Ferney
+		</h1>
+
+
+
+<img alt="grass overlay" src={grass} class="w-full absolute bottom-0"/>
+
 </div>
 
-<div>
-	{state}
+<div class="absolute bottom-[20px] w-full">
+	<div class="flex flex-col w-full items-center justify-center gap-5">
+
+
+		<div class="flex flex-row gap-4">
+			<button class="bg-green-500 text-white font-bold py-2 px-4 rounded shadow-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-400" on:click={transcribeButtonPress}>Transcribe speech</button>
+			<button class="bg-green-500 text-white font-bold py-2 px-4 rounded shadow-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-400" on:click={begin}>Begin conversation</button>
+		</div>
+
+		<div class="text-1xl text-white">
+			State: {state}
+		</div>
+	</div>
 </div>
 
-<button on:click={transcribeButtonPress}>Transcribe</button>
-<button on:click={begin}>Begin</button>
 
-<audio bind:this={audioPlayer} src="{audioSrc}" controls></audio>
+
+
+
+<!--<audio bind:this={audioPlayer} src="{audioSrc}" controls></audio>-->
 
 <script>
+	import grass from '$lib/images/grass.png'
 	import { onMount } from 'svelte';
 	import { getMediaRecorder, tryTranscribeSpeech, playAudio } from './audio.js';
 	import { transcribedTextStore, appStateStore } from "./stores.js";
